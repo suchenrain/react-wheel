7a65720c431e3eaffbdb9a8a7f164abf3754106f
suchenxiaoyu <suchenxiaoyu@gmail.com>
2019-07-08 19:59:13 +0800

    create component

---
+++ comment-app/src/components/CommentInput.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react'
+
+class CommentInput extends Component {
+    render() {
+        return (
+            <div>CommentList</div>
+        )
+    }
+}
+
+export default CommentInput
